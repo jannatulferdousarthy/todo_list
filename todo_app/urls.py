88ab04adc931087f1ec8b05be1cad454ea73e995
jannatulferdousarthy <arthyjannatul@gmail.com>
2023-09-08 19:03:20 +0600

    first commit

---
+++ todo_app/urls.py
@@ -0,0 +1,27 @@
+urlpatterns = [
+
+]
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+
+    path("", views.ListListView.as_view(), name="index"),
+
+]
+
+from todo_app import views
+
+
+urlpatterns = [
+
+    path("",
+
+        views.ListListView.as_view(), name="index"),
+
+    path("list/<int:list_id>/",
+
+        views.ItemListView.as_view(), name="list"),
+
